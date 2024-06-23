2526109c75bd155c53dd71016305c2e0c1e0edbd
Jeevan kumar <jeevankumarkorra2005@gmail.com>
2024-06-23 18:58:16 +0530

    design change

--- src/components/News.js
+++ src/components/News.js
@@ -1,23 +1,21 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./spinner/Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
+import './News.css';
 
-const News = (props) =>{
-const [articles,setArticles] = useState([]);
-const [loading,setLoading] = useState(true);
-const [page,setPage] = useState(1);
-const [totalResults,setTotalResults] = useState(0);
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-
- const capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  
-   
-  const UpdateNews = async () =>{
+  const UpdateNews = async () => {
     props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
@@ -26,90 +24,72 @@ const [totalResults,setTotalResults] = useState(0);
     props.setProgress(50);
     let parsedData = await data.json();
     props.setProgress(70);
-    console.log(parsedData);
     setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
     props.setProgress(100);
-  }
-  
-useEffect(() => {
-  document.title = `${capitalizeFirstLetter(props.category)} - Pegio-News`;
-  UpdateNews();
-  // eslint-disable-next-line
-}, [])
+  };
 
-
+  useEffect(() => {
+    document.title = `${capitalizeFirstLetter(props.category)} - Pegio-News`;
+    UpdateNews();
+  }, []);
 
   const fetchMoreData = async () => {
-    setPage(page+1);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
     setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     setArticles(articles.concat(parsedData.articles));
     setTotalResults(parsedData.totalResults);
     setLoading(false);
-  }
-
-  // const handlePrevClick = async () => {
-  //   setPage(page-1);
-  //   UpdateNews();
-  // }
-
-  // const handleNextClick = async () => {
-  //   setPage(page+1);
-  //   UpdateNews();
-  // }
- return (
-      <>
-        <h1 className="text-center p-4 " style={{marginTop:"50px"}}>
-          Pegio-News - Top {capitalizeFirstLetter(props.category)}{" "}
-          Headlines
-        </h1>
-        {loading && <Spinner/>}
-
-        <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-        >
-          <div className="row mx-5">
-            {articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-          </div>
-         
-        </InfiniteScroll>
-      </>
-    )
-  }
-
-
-  News.defaultProps = {
-    country: "in",
-    pageSize: 1,
-    category: "general",
-  };
- News.propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
   };
 
+  return (
+    <>
+      <h1 className="text-center p-4" style={{ marginTop: "50px" }}>
+        Pegio-News - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="news-container">
+          {articles.map((element) => {
+            return (
+              <div className="news-card" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 1,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
 export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,33 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './NewsItem.css';
 
-
-const NewsItem = (props) =>{
-
-    let {title, description ,imageUrl ,newsUrl,author,date ,source} = props;
-    return (
-     <div className='my-3'>
-      <div className="card" >
-        <div style={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-          position: 'absolute',
-          right: '0',
-          margin: '5px'
-        }}>
-      <span className=" badge rounded-pill bg-danger " >{source}</span>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="news-item my-3">
+      <div className="card">
+        <div className="badge-container">
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img src={imageUrl ? imageUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPjs0rKvYAeL85hJXV96n27E_Yc7IGIgM5Yg&s"} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text"><small className="text-body-secondary">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+          <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">View more</a>
+        </div>
       </div>
-  <img src={imageUrl ? imageUrl :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPjs0rKvYAeL85hJXV96n27E_Yc7IGIgM5Yg&s" } className="card-img-top" alt="..." />
-  
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <p class="card-text"><small class="text-body-secondary">By {!author ?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
-    <a href={newsUrl} target='_blank' className=" btn btn-sm btn-dark">view more</a>
-  </div>
-</div>
-     </div>
-    )
-  }
+    </div>
+  );
+};
 
-
-export default NewsItem
+export default NewsItem;
